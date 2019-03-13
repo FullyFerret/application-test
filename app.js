a0281858e5f2b7aa6fffb1aff0c241b8570fb30e
Eric Amshukov <eamshukov@gmail.com>
2019-03-13 15:47:26 -0400

    inline php; escaping; responsiveness

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
 /* application-test - app.js
    Forked by Eric Amshukov */
 
-const entityMap = Object.freeze({
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': "&quot;",
-  "'": "&#39;",
-  "/": "&#x2F;",
-  "`": "&#x60;",
-  "=": "&#x3D;"
-});
-
-/* Properly escape data to prevent XSS */
-const escapeHtml = function(str) {
-  return String(str).replace(/[&<>"'`=\/]/g, function(s) {
-    return entityMap[s];
-  });
-};
-
 $(function() {
-  $.fn.extend({
-    escapeHTML: function() {
-      return escapeHtml(this.html());
-    }
-  });
-
   $("button").on("click", function() {
     const name = $(this)
         .closest("tr")
         .find(".first_name")
-        .escapeHTML(),
+        .html(),
       email = $(this)
         .closest("tr")
         .find(".email")
-        .escapeHTML();
+        .html();
 
     /* Non-blocking Bootstrap Modal Alert */
     BootstrapModalWrapperFactory.alert({
--- index.php
+++ index.php
@@ -12,63 +12,63 @@ $people = array(
     array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
  );
 
- /* Reduce people to <tr> rows */
-$rows = array_reduce($people, function($rows, $row) {
-   $columns = "";
-
-   /* Grab each column and key information for selecting */
-   foreach($row as $k => $column){
-      $columns .= "<td class='$k'>$column</td>";
-   }
-   return $rows . 
-            "<tr>" .
-               $columns . 
-               "<td>
-                  <button class='btn btn-outline-primary'>Alert</button>
-               </td>" .
-            "</tr>";
-});
-
-/* Retrieve keys header cells assuming each person has identical sets of keys */
-$headerCells = empty($people[0]) ? "" : array_reduce(array_keys($people[0]), function($cells, $cell) {
-   return $cells . "<th>$cell</th>";
-});
-
-/* Construct table from created rows */
-$table = "<table class='table table-hover table-borderless fadeIn animated'>
-            <caption>List of people</caption>
-
-            <thead>
-               <tr>$headerCells</tr>
-            </thead>
-            <tbody>$rows</tbody>
-         </table>";
 ?>
 
 <!doctype html>
  <html>
    <head>
+      <title>People</title>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" integrity="sha256-HtCCUh9Hkh//8U1OwcbD8epVEUdBvuI8wj1KtqMhNkI=" crossorigin="anonymous" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-      <title>People Table</title>
 
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap-modal-wrapper@1.0.16/dist/bootstrap-modal-wrapper-factory.min.js" integrity="sha256-U4z/KrjsWdHkqkQuhH8u4ph+EzRjzNnAlDjqYSjMcns=" crossorigin="anonymous"></script>
       <script src="app.js"></script>
+
    </head>
 
    <body>
       <div class="container">
          <header class="row">
-            <h2 class="pb-2 mt-4 mb-5 border-bottom mx-auto">People Table</h2>
+            <h2 class="pb-2 mt-4 mb-5 border-bottom mx-auto">People</h2>
          </header>
          <section class="row">
             <div class="col-sm-12">
-               <?php echo $table; ?>
+               <table class='table table-hover table-borderless fadeIn animated'>
+                  <caption><span class="mr-2"><i class="fas fa-users"></i></span><span class="d-none d-md-inline-block"> List of people</span></caption>
+                  <thead>
+                     <tr>
+                        <!-- Assuming everyone will have the same information -->
+                        <?php if (!empty($people)) foreach (array_keys($people[0]) as $k => $cellHeader) { ?>
+                           <th class="<?= $k ?>"><?= $cellHeader ?></th>
+                        <?php } ?>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php foreach ($people as $person) { ?>
+                        <tr>
+                           <?php foreach ($person as $k => $column) { ?>
+                              <td class="<?= $k ?>"><?= htmlspecialchars($column, ENT_QUOTES, 'UTF-8') ?></td>
+                           <?php } ?>
+                           <td>
+                              <button class='btn btn-outline-primary'><span class="mr-2"><i class="far fa-bell"></i></span><span class="d-none d-md-inline-block"> Alert</span></button>
+                           </td>
+                        </tr>
+                     <?php } ?>
+                  </tbody>
+               </table>
             </div>
          </section>
+         <noscript>
+            <section class="row">
+               <div class="col-sm-12">
+                  <div class="alert alert-warning fadeIn animated"><i class="fas fa-exclamation-triangle fa-fw"></i> Some things won't run without JavaScript, please enable it and refresh the page.</div>
+               </div>
+            </section>
+         </noscript>
       </div>
    </body>
  </html>
